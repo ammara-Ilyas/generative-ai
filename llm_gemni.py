427e66f4c929342e9660024c2bddf9c2e13de6be
ammara-ilyas <ismarailyas545@gmail.com>
2024-09-28 20:08:31 +0000

    initial commit

--- llm_gemni.py
+++ llm_gemni.py
@@ -1,2 +1,13 @@
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant that translates {input_language} to {output_language}.",
+        ),
+        ("human", "{input}"),
+    ]
+)
